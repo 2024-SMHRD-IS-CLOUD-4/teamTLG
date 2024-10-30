416216cc8e1664dae58d5a3c8bf48d6bee57e1d6
smhrd <smhrd@192.168.219.48>
2024-10-30 14:42:56 +0900

    오타 수정 - 김의진 14:42

--- TravelMate/src/main/java/com/tlg/controller/LoginController.java
+++ TravelMate/src/main/java/com/tlg/controller/LoginController.java
@@ -21,32 +21,24 @@ public class LoginController extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
-		
-		
+				
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		
-		
+			
 		TmMember loginMember = new TmMember(id, pw);
 		
-
 		MemberDAO dao = new MemberDAO();
 		
 		TmMember result = dao.login(loginMember);
-		
-		
+				
 		if(result == null) {
-			// 로그인 실패
 			response.sendRedirect("login.html");
 		}else {
-			// 세션에다가 회원 정보를 저장하고, index.jsp로 사용자를 보내주기!
 			HttpSession session = request.getSession();
 			session.setAttribute("member", result);
 			response.sendRedirect("mainPage.html");
 		}
-		
-		
-	
+			
 	}
 
 }
