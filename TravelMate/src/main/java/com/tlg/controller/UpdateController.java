15be1d525d19d0613e192553d034f8707490b10b
smhrd <smhrd@192.168.219.48>
2024-11-05 12:40:53 +0900

    회원정보 수정 탈퇴 - 김의진

---
+++ TravelMate/src/main/java/com/tlg/controller/UpdateController.java
@@ -0,0 +1,41 @@
+package com.tlg.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tlg.model.MemberDAO;
+import com.tlg.model.TmMember;
+
+@WebServlet("/UpdateController")
+public class UpdateController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String nick = request.getParameter("nick");
+		
+		TmMember updateMember = new TmMember(id, pw, nick);
+		
+		MemberDAO dao = new MemberDAO();
+		
+		int result = dao.update(updateMember);
+		
+		if(result > 0) {
+			HttpSession session = request.getSession();
+			session.setAttribute("member", updateMember);
+			response.sendRedirect("mainPage.jsp");
+		} else {
+			response.sendRedirect("myPageScript.jsp");
+		}
+	}
+
+}
--- TravelMate/src/main/java/com/tlg/model/TmMember.java
+++ TravelMate/src/main/java/com/tlg/model/TmMember.java
@@ -15,12 +15,16 @@ public class TmMember {
 	String nick;
 	String gender;
 	String email;
-	
+
 	public TmMember(String id, String pw) {
 		this.id = id;
 		this.pw = pw;
 	}
 
-
+	public TmMember(String id, String pw, String nick) {
+		this.id = id;
+		this.pw = pw;
+		this.nick = nick;
+	}
 	
 }
