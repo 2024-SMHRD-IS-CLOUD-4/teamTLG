8f6c92c5db3798f2b6534115064f93a9ba5acfe1
smhrd <smhrd@192.168.219.70>
2024-11-11 10:47:24 +0900

    voteVO 수정 - 롬복으로 처리 유란

--- TravelMate/src/main/java/com/tlg/model/Vote.java
+++ TravelMate/src/main/java/com/tlg/model/Vote.java
@@ -2,10 +2,13 @@ package com.tlg.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Setter
 @Getter
 @AllArgsConstructor
-
 public class Vote {
     private int choiceIdx;
     private int trIdx;
@@ -13,56 +16,4 @@ public class Vote {
     private String voteCont;
     private int voteScore;
 
-    // 기본 생성자
-    public Vote() {}
-
-    // 필드를 초기화하는 생성자
-    public Vote(int choiceIdx, int trIdx, int scheIdx, String voteCont, int voteScore) {
-        this.choiceIdx = choiceIdx;
-        this.trIdx = trIdx;
-        this.scheIdx = scheIdx;
-        this.voteCont = voteCont;
-        this.voteScore = voteScore;
-    }
-
-    // Getters and setters
-    public int getChoiceIdx() {
-        return choiceIdx;
-    }
-
-    public void setChoiceIdx(int choiceIdx) {
-        this.choiceIdx = choiceIdx;
-    }
-
-    public int getTrIdx() {
-        return trIdx;
-    }
-
-    public void setTrIdx(int trIdx) {
-        this.trIdx = trIdx;
-    }
-
-    public int getScheIdx() {
-        return scheIdx;
-    }
-
-    public void setScheIdx(int scheIdx) {
-        this.scheIdx = scheIdx;
-    }
-
-    public String getVoteCont() {
-        return voteCont;
-    }
-
-    public void setVoteCont(String voteCont) {
-        this.voteCont = voteCont;
-    }
-
-    public int getVoteScore() {
-        return voteScore;
-    }
-
-    public void setVoteScore(int voteScore) {
-        this.voteScore = voteScore;
-    }
 }
